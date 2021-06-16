8d9b189fa8d01799cdc87c9b90c7cb8da425abe7
Alexandre <70768800+Paniniale@users.noreply.github.com>
2021-06-16 20:45:02 -0300

    Run

---
+++ Firebase/app.js
@@ -0,0 +1,17 @@
+const blocslist = document.querySelector('[data-js="Blocs-List"]')
+const db = firebase.firestore()
+
+db.collection('Blocos').get()
+    .then( snapshot => {
+       const blocs =  snapshot.docs.reduce((acc,doc)=> {
+           const {Titulo, Texto, Data} = doc.data()
+            acc += `<li class="listblocs">
+            <h5>${Titulo}</h5>
+            <p> Adicionado em ${Data}</p>
+            <p> ${Texto}</p>
+            </li>`
+            return acc
+        },'')
+        blocslist.innerHTML = blocs
+    })
+    .catch(err => {console.log(err.message)})
