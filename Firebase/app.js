60b01139110d8f7c438206de5f21178e855f7e81
Alexandre <70768800+Paniniale@users.noreply.github.com>
2021-06-16 23:26:41 -0300

    23:26

--- Firebase/app.js
+++ Firebase/app.js
@@ -1,4 +1,5 @@
 const blocslist = document.querySelector('[data-js="Blocs-List"]')
+const btnadd = document.querySelector('[data-js="adiciona"]')
 const db = firebase.firestore()
 
 db.collection('Blocos').get()
@@ -7,14 +8,34 @@ db.collection('Blocos').get()
            const {Titulo, Texto, Data} = doc.data()
             acc += `<li class="listblocs">
             <h5>${Titulo}</h5>
-            <p> Adicionado em ${Data}</p>
+            <p> Adicionado em ${Data} </p>
             <p> ${Texto}</p>
             </li>`
             return acc
         },'')
+
         blocslist.innerHTML = blocs
     })
     .catch(err => {console.log(err.message)})
-    
 
+
+console.log(btnadd)
+
+btnadd.addEventListener('submit', saveDB )
+
+
+function saveDB(e) {
+    e.preventDefault()
     
+    db.collection('Blocos').add({
+        Titulo: e.target.Titulo.value,
+        Texto: e.target.Texto.value,
+        Data: '15 de abril'
+    })
+    .then(() => {
+        Console.log("Nota Adicionada")
+    })
+    .catch(() => {
+        Console.log("Nota Não Adicionada")
+    })
+}
